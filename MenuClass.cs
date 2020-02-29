fd2bd367e956a9b8821d42b9b8b921dd361b7c5e
Leo Esson <leo.esson@yahoo.com>
2020-02-29 12:44:31 +0100

    Error messages

--- MenuClass.cs
+++ MenuClass.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 
-public class Menu
+//public class Menu
 {
-	public void Start
+	//public void Start
 	{
-		 int choice = -1;
-		 while (choice !=0)
-		 {
-			WriteMenuText(); //show the menu options and pick up user's input
-			choice = int.Parse(Console.ReadLine()); //create an instance of chosen class
-			switch (choice) //apply statement to all cases
+		 //int choice = -1;
+		 //while (choice != 0)
+		{
+			//WriteMenuText(); //show the menu options and pick up user's input
+			//choice = int.Parse(Console.ReadLine()); //create an instance of chosen class
+			//switch (choice) //apply statement to all cases
 			{
-			case 1:  //menu item 1
+			//case 1:  //menu item 1
 				{
-				WholeNumbersForAdd sumObj = new WholeNumbersForAdd(); //declare a local reference variable and create an instance of it
-				sumObj.Start(); //call its start method
-				break;
+				//WholeNumbersForAdd sumObj = new WholeNumbersForAdd(); //declare a local reference variable and create an instance of it
+				//sumObj.Start(); //call its start method
+				//break;
 				}//case 1
 			}//switch
-		 }//while loop 
+		}//while loop 
 
 	}//start
 }//class menu
--- TempConverter.cs
+++ TempConverter.cs
@@ -4,38 +4,42 @@ public class TemperatureConverter
 {
 	public void Start()
 	{
-		int choice = 0
-			do
+		int choice = 0;
+		do
 			{
 			DisplayMenu();
 			choice = Input.ReadIntegerConsole("Your choice:  ");
 			switch (choice)
+			{
 				case 1:
-					CalculateFarenheitToCelcius( );
+					CalculateFarenheitToCelcius();
 					break;
 				case 2:
-					CalculateCelciusToFarenheit( );
+					CalculateCelciusToFarenheit();
 					break;
 				case 0: //exits loop, return to menu
 					break;
 				default:
-					Console.WriteLine ("Error: invalid option. Please select 1, 2, or 0.")
+					Console.WriteLine("Error: invalid option. Please select 1, 2, or 0.");
 					break;
+			}//switch
 			}//do
-			while (choice != 0); //while
+		while (choice != 0); //while
 
 	}//start
 
 	public void DisplayMenu()
     {
 			Console.WriteLine("************************************************************");
-			Console.WriteLine("MENU");
+			Console.WriteLine("MAIN MENU");
 			Console.WriteLine("************************************************************");
-			Console.WriteLine("Convert Farenheit to Celcius: 1);
-			Console.WriteLine("Convert Celcius to Farenheit: 2);
-			Console.WriteLine("Exit the converter and return to main menu: 0");
+			Console.WriteLine("Convert Farenheit to Celcius: 1");
+			Console.WriteLine("Convert Celcius to Farenheit: 2");
+			Console.WriteLine("Exit the converter: 0");
 			Console.WriteLine("************************************************************");
-			Console.WriteLine("Your selection: ");
+			choice = Console.ReadLine();
+			Console.WriteLine("Your selection: +choice+ "); 
+			choice = Console.ReadLine();
 		}//displaymenu
 
 }//class tempconverter
